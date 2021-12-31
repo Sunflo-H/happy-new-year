4a88b4d43d07fc5c7ac351d0c02cfc2ffca30ab4
Sunflo-H <sunflo0524@gmail.com>
2022-01-01 05:18:13 +0900

    header, border test

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const add = document.querySelector('.add');
 const del = document.querySelector('.del');
 const header = document.querySelector('header');
+const img = header.querySelector('img');
 add.addEventListener('click',animation);
 del.addEventListener('click',delAnimation);
 function animation(){
-    header.classList.add('animation');
+    img.classList.add('animation');
     console.log('실행');
 }
 
 function delAnimation(){
-    header.classList.remove('animation');
+    img.classList.remove('animation');
     console.log('실행');
 }
