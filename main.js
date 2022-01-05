a846128744e5a4460a691fb7df37c61cbf88bcba
sunflo-h <sunflo0524@gmail.com>
2022-01-06 03:12:51 +0900

    완성? 모바일 사이즈테스트 전

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-const add = document.querySelector('.add');
-const del = document.querySelector('.del');
-const header = document.querySelector('header');
-const img = header.querySelector('img');
-add.addEventListener('click',animation);
-del.addEventListener('click',delAnimation);
-function animation(){
-    img.classList.add('animation');
-    console.log('실행');
-}
+let bokImage = document.querySelector('main>img');
+let texts = document.querySelectorAll('main>p');
+console.log(bokImage);
 
-function delAnimation(){
-    img.classList.remove('animation');
-    console.log('실행');
-}
+setTimeout(function(){
+    bokImage.classList.remove('animation-image-move');
+    bokImage.classList.add('animation-image-open');
+    texts.forEach(text=>text.classList.add('animation-text'));
+    
+},1500);
