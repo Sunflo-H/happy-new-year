222b31598c477855f547027299e599f1cad45725
sunflo-h <sunflo0524@gmail.com>
2022-01-06 03:18:41 +0900

    완성!

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let bokImage = document.querySelector('main>img');
 let texts = document.querySelectorAll('main>p');
-console.log(bokImage);
+let newYearImage = document.querySelector('header>img');
 
 setTimeout(function(){
     bokImage.classList.remove('animation-image-move');
     bokImage.classList.add('animation-image-open');
     texts.forEach(text=>text.classList.add('animation-text'));
-    
+    newYearImage.classList.add('animation-image');
 },1500);
